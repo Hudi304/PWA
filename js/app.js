4f28b5f1e0c5e2a88bc5137f1562bc0ee531d06c
Laurentiuu <laurgales@yahoo.com>
2022-10-24 16:11:26 +0300

    blanitz

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 $(function () {
   "use strict";
 
-
   //serviceworker registration
 
   window.addEventListener("load", (e) => {
@@ -19,7 +18,6 @@ $(function () {
       document.querySelector(".alert").removeAttribute("hidden");
     }
   }
-
 });
 
 // mCustomScrollbar initialization
@@ -37,3 +35,67 @@ $(function () {
     })
     .trigger("resize");
 })(jQuery);
+
+function createButton(text, href, icon) {
+  var button1 = document.createElement("button");
+  var text1 = document.createTextNode(text);
+
+  let i1 = document.createElement("i");
+  let class1 = document.createAttribute("class");
+  class1.value = icon;
+  i1.setAttributeNode(class1);
+
+  let a1 = document.createElement("a");
+  let href1 = document.createAttribute("href");
+  href1.value = href;
+  a1.setAttributeNode(href1);
+
+  button1.appendChild(i1);
+  button1.appendChild(text1);
+
+  a1.appendChild(button1);
+
+  var element = document.getElementById("wrapper");
+  element.appendChild(a1);
+}
+
+function createButtonWithClick(text, href, icon, onClick) {
+  var button1 = document.createElement("button");
+  var text1 = document.createTextNode(text);
+
+  let i1 = document.createElement("i");
+  let class1 = document.createAttribute("class");
+  class1.value = icon;
+  i1.setAttributeNode(class1);
+
+  let a1 = document.createElement("a");
+  let href1 = document.createAttribute("href");
+  href1.value = href;
+
+  a1.addEventListener("click", onClick)
+  a1.setAttributeNode(href1);
+
+  button1.appendChild(i1);
+  button1.appendChild(text1);
+
+  a1.appendChild(button1);
+
+  var element = document.getElementById("wrapper");
+  element.appendChild(a1);
+}
+
+function createInstallButton(text) {
+  var button1 = document.createElement("button");
+  var text1 = document.createTextNode(text);
+  button1.appendChild(text1);
+  var element = document.getElementById("wrapper");
+  element.appendChild(button1);
+}
+
+
+
+function openSMSMobile(e) {
+  console.log("AICIII");
+  window.open("sms:0754302599&body=hello", "_self");
+  return false;
+}
